7d80bbf705bcf87946b29fd4b43befe7266675ad
Aditi Rai <aditi45@gmail.com>
2021-03-01 21:36:54 -0500

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,6 +24,19 @@ button.on("click", function(){
 	console.log(filteredData);
 });
 
+	data.forEach(function(weatherReport) {
+	console.log(weatherReport);
+	var row = tbody.append("tr");
+	Object.entries(weatherReport).forEach(function([key, value]) {
+	  console.log(key, value);
+	  // Append a cell to the row for each value
+	  // in the weather report object
+	  var cell = row.append("td");
+	  cell.text(value);
+	});
+  });
+  
+
 // //Populate table
 // populate(data);
 
